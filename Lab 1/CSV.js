0c17a982962a8763cedd5e0884370e0866f01270
akansha-mehrotra <92391901+akansha-mehrotra@users.noreply.github.com>
2023-04-10 10:43:34 +0530

    Add files via upload

---
+++ Lab 1/CSV.js
@@ -0,0 +1,1 @@
+mongoimport --db Student --collection airlines --type csv --headerline --file /home/hduser/Desktop/airline.csv
---
+++ Lab 1/Lab1.js
@@ -0,0 +1,13 @@
+//use myDB;
+// db.createCollection("Students");
+// db.Students.insert({Rollno: 1, Name: "ABC",Age:7,ContactNo:1928374650,Email:"abc@gmail.com"});
+
+// var mystudents=[{Rollno:10,Name: "BCD",Age:10,ContactNo:6172839450,Email:"bcd@gmail.com"},{Rollno:11,Name: "CDE", Age:10,ContactNo:6172635450,Email:"cde@gmail.com"}]
+// db.Students.insert(mystudents)
+
+// db.Students.update({Rollno:11,Name:"BCD",Age:10,ContactNo:6172839450},{$set:{Email:"xyz@gmail.com"}},(upsert:true));
+
+
+// db.Students.count();
+
+// db.Students.drop();
